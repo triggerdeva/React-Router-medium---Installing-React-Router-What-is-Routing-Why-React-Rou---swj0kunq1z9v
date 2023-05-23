d16dc5036b202a111861424c19146fbc68b67630
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-23 22:52:01 +0530

    Update BackgroundColorChanger.js

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import { useParams } from 'react-router-dom';
 
 export const BackgroundColorChanger = () =>{
+    const params = useParams();
+    console.log(params.colorname);
     return(
-       
+        <main style={{backgroundColor: `${params.colorname}`, height: '100vh'}}>
+            Background Color Changer
+        </main>
     )
 }
